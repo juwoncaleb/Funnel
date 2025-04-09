20c67a7ff5e18e5152336801c37167a51669954c
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2025-04-10 00:42:11 +0100

    Update Book.js

--- pages/Book.js
+++ pages/Book.js
@@ -16,30 +16,25 @@ export default function Review() {
   }, []);
   return (
     <div className="">
-      <div className="review_land flex items-center justify-center min-h-screen">
-        <div className="text-center">
-          <p className="testimonila_header rev_head text-white">
+      <div className="vsl_div">
+        <center>
+          <p className=" AGent_text text-white">
             Every successful Agent, we worked with started just like <br />
             <span className="salescall">YOU</span>
           </p>
-          <div className="relative pt-[45.3%]">
-            <iframe
-              src="https://player.vimeo.com/video/1070872120?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-              frameBorder="0"
-              allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-              className="absolute top-0 left-0 w-full h-full"
-              title="Richard"
-              style={{
-                border: "none", // Remove the iframe border
-                maxWidth: "100%", // Ensure responsiveness
-                height: "100%", // Make sure the video takes full height
-              }}
-            ></iframe>
-          </div>
-          <button onClick={handleScroll} className="next_btn mt-4">
-            Book a Call
-          </button>{" "}
-        </div>
+          <div>
+              <iframe
+                className="video"
+                src="https://killerplayer.com/watch/video/7ad7082b-e455-4e0b-ab68-1e7a81012276"
+                frameBorder="0"
+                allow="autoplay; gyroscope; picture-in-picture;"
+                allowFullScreen
+              ></iframe>
+            </div>
+            <button onClick={handleScroll} className="next_btn mt-10 mb-14">
+              Book a Call
+            </button>{" "}
+        </center>
       </div>
       <div className="rest_review">
         <center>
@@ -60,17 +55,7 @@ export default function Review() {
           data-url="https://calendly.com/calebomojuwon/quick-call"
           style={{ minWidth: "320px", height: "700px" }}
         ></div>
-        <center>
-          <p className="retainer text-center">
-            No Monthly <span className="importance">PAYMENT</span> - We install
-            this System
-            <br /> Once{" "}
-          </p>
-          <button onClick={handleScroll} className="next_btn mt-10 mb-14">
-            Book a Call
-          </button>{" "}
-        </center>
-        <InfiniteCarousel />
+
         <div className="caution flex justify-center gap-20">
           <img className="book" src="./right.png" />
           <div className="cautpn">
@@ -86,71 +71,24 @@ export default function Review() {
             </button>{" "}
           </div>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 process">
-          <div>
-            <div className="flex flex-col items-center gap-2">
-              <img
-                width="50"
-                height="50"
-                src="https://img.icons8.com/ios/50/FA5252/ophthalmology.png"
-                alt="ophthalmology"
-              />
-              <p className="steps_head text-center">Discovery</p>
-            </div>
-            <p className="card_txt text-center">
-              You are currently learning how we help realtors. We want you to
-              binge-watch our content and grasp our process to see if it is the
-              right fit for you.
-            </p>
-          </div>
-
-          <div>
-            <div className="flex flex-col items-center gap-2">
-              <img
-                width="50"
-                height="50"
-                src="https://img.icons8.com/ios-filled/50/FA5252/phone-disconnected.png"
-                alt="phone-disconnected"
-              />
-              <p className="steps_head text-center">Schedule Call</p>
-            </div>
-            <p className="card_txt text-center">
-              We have a 30-45 minute call where we ask questions to understand
-              your goals and see if we are a good fit
-            </p>
-          </div>
-
-          <div>
-            <div className="flex flex-col items-center gap-2">
-              <img
-                width="50"
-                height="50"
-                src="https://img.icons8.com/ios-filled/50/FA5252/groups.png"
-                alt="groups"
-              />
-              <p className="steps_head text-center">Onboarding</p>
-            </div>
-            <p className="card_txt text-center">
-              You meet the team, and we walk you through the different phases we
-              would take to help you achieve your goals.
-            </p>
-          </div>
-        </div>
-
-        <div className="w-[70%] h-[400px] bg-[#0C0C0C] flex items-center justify-center mx-auto deal">
-          <div className="flex flex-col items-center">
-            <h1 className="text-white text-[20px] md:text-[40px] text-center font-semibold">
-              You never have to worry where your next deal is coming from?
-            </h1>
-            <p className="time">
-              Time is money . Let us help you spend it on closing deals!
-            </p>
-            <button onClick={handleScroll} className="next_btn mt-10 mb-14">
+           <div className="bg-white screenshot">
+                <center>
+                  <p className="testimonila_header rev_head text-white text-center">
+                    <span className="text-white mb-20">TESTIMONIAL SCREENSHOTS </span>
+                  </p>{" "}
+                </center>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center tts mt-14 px-4">
+                  <img className="test_Card" src="./a1.png" alt="testimonial" />
+                  <img className="test_Card" src="./a2.png" alt="testimonial" />
+                  <img className="test_Card" src="./a6.png" alt="testimonial" />
+                  <img className="test_Card" src="./luka.jpg" alt="testimonial" />
+                </div>
+                <center>
+                <button onClick={handleScroll} className="next_btn mt-10 mb-14">
               Book a Call
             </button>{" "}
-          </div>
-        </div>
+                </center>
+              </div>
       </div>
     </div>
   );
